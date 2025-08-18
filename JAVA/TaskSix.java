3d41f168bfeb8d9585c77efdf3980c9ffba0038c
Delight44-max <eboagorojahdiebube@gmail.com>
2025-08-18 15:53:55 +0100

    added

---
+++ JAVA/TaskSix.java
@@ -0,0 +1,25 @@
+public class TaskSix{
+
+public static void main(String[] args){
+
+int numbers  = {10, 20, 30, 40};
+largest = 0;
+for(int count = 0; count < numbers.length; count++){
+
+if(numbers[count] > largest) {
+largest = numbers[count];
+
+}
+
+System.out.print(largest);
+
+}
+
+
+
+
+
+
+
+}
+}
