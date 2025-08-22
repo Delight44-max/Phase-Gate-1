96a5e3129efdbd13553d2a2f9e3f62a729f45968
Delight44-max <eboagorojahdiebube@gmail.com>
2025-08-22 16:41:29 +0100

    added

---
+++ PYTHON/DAY5/parking_Slot.py
@@ -0,0 +1,7 @@
+# determine the length of the spaces
+# compare the length of spaces with the postions, if the postion is 
+# equal to the number of spaces, show a message "the position has been occupied"
+# if its not equal then display " there is space left". 
+
+
+
