1d3a82ac9234117b523bceabdcdd03bb31d9f1df
Delight44-max <eboagorojahdiebube@gmail.com>
2025-08-23 16:29:54 +0100

    in progress

---
+++ JAVA/MenstrualAppCalculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+
+public class MenstrualAppCalculator {
+
+Scanner input = new Scanner(System.in);
+
+public static String MenstrualDetails() {
+
+System.out.println("INPUT THE START DATE OF YOUR LAST PERIOD(yyyy-MM-dd): ");
+String userinput = input.nextLine();
+
+LocalDate startDate = LocalDate.parse(userinput);  
+
+
+
+
+
+
+
+
+
+
+} //method
+
+
+
+
+
+
+
+
+
+
+
+} //class
