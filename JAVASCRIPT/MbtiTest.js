b63628d683c82c3a1cb528985e8beced175fd0f5
Delight44-max <eboagorojahdiebube@gmail.com>
2025-08-25 19:37:29 +0100

    added

---
+++ JAVASCRIPT/MbtiTest.js
@@ -0,0 +1,474 @@
+const prompt = require('prompt-sync')();
+
+function allQuestions() {
+console.log("FREE PERSONALITY TEST\n");
+console.log("Please select (A) or (B)\n");
+
+  
+console.log("   ");
+
+
+let questionOne = console.log("1. You regularly make new friends.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerOne = prompt("Your answer: ").toUpperCase();
+
+
+if (answerOne === "A") {
+console.log("You selected: AGREE");
+} else if (answerOne === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+return;
+}
+
+
+console.log("   ");
+
+
+const questionTwo = console.log("2. You can easily connect with people you have just met.");
+
+
+
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerTwo = prompt("Your answer: ").toUpperCase();
+
+if (answerTwo === "A") {
+console.log("You selected: AGREE");
+} else if (answerTwo === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+return;
+}
+
+
+console.log("   ");
+
+
+
+const questionThree = console.log("3. If your plans are interrupted, your top priority is to get back on track as soon as possible.");
+
+
+
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerThree = prompt("Your answer: ").toUpperCase();
+
+if (answerThree === "A") {
+console.log("You selected: AGREE");
+} else if (answerThree === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+return;
+}
+
+
+console.log("   ");
+
+
+const questionFour = console.log("4. You are still bothered by mistakes that you made a long time ago.")
+
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerFour = prompt("Your answer: ").toUpperCase();
+
+if (answerFour === "A") {
+console.log("You selected: AGREE");
+} else if (answerFour === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+return;
+}
+
+
+
+console.log("   ");
+
+
+
+const questionFive = console.log("5. Your emotions control you more than you control them.");
+
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerFive = prompt("Your answer: ").toUpperCase();
+
+if (answerFive === "A") {
+console.log("You selected: AGREE");
+} else if (answerFive === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+console.log("   ");
+
+const questionSix = console.log("6. You would love a job that requires you to work alone most of the time.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerSix = prompt("Your answer: ").toUpperCase();
+
+if (answerSix === "A") {
+console.log("You selected: AGREE");
+} else if (answerSix === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+console.log("   ");
+
+
+
+const questionSeven = console.log("7. You believe that pondering abstract philosophical questions is a waste of time.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerSeven = prompt("Your answer: ").toUpperCase();
+
+if (answerSeven === "A") {
+console.log("You selected: AGREE");
+} else if (answerSeven === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+console.log("   ");
+
+
+const questionEight = console.log("8. You feel more drawn to busy, bustling atmospheres than to quiet, intimate places.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerEight = prompt("Your answer: ").toUpperCase();
+
+if (answerEight === "A") {
+console.log("You selected: AGREE");
+} else if (answerEight === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+console.log("   ");
+
+const questionNine = console.log("9. If a decision feels right to you, you often act on it without needing further proof.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerNine = prompt("Your answer: ").toUpperCase();
+
+if (answerNine === "A") {
+console.log("You selected: AGREE");
+} else if (answerNine === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+
+console.log("   ");
+
+
+const questionTen = console.log("10. You often feel overwhelmed.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerTen = prompt("Your answer: ").toUpperCase();
+
+if (answerTen === "A") {
+console.log("You selected: AGREE");
+} else if (answerTen === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+console.log("   ");
+
+
+const questionEleven = console.log("11. You complete things methodically without skipping over any steps.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerEleven = prompt("Your answer: ").toUpperCase();
+
+if (answerEleven === "A") {
+console.log("You selected: AGREE");
+} else if (answerEleven === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+   console.log("   ");
+
+const questionTwelve = console.log("12. You prefer tasks that require you to come up with creative solutions rather than follow concrete steps.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerTwelve = prompt("Your answer: ").toUpperCase();
+
+if (answerTwelve === "A") {
+console.log("You selected: AGREE");
+} else if (answerTwelve === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+
+
+
+ console.log("   ");
+
+const questionThirteen = console.log("13. You are more likely to rely on emotional intuition than logical reasoning when making a choice.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerThirteen = prompt("Your answer: ").toUpperCase();
+
+if (answerThirteen === "A") {
+console.log("You selected: AGREE");
+} else if (answerThirteen === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+
+
+
+console.log("   ");
+
+const questionFourtheen = console.log("14. You struggle with deadlines.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerFourtheen = prompt("Your answer: ").toUpperCase();
+
+if (answerFourtheen === "A") {
+console.log("You selected: AGREE");
+} else if (answerFourtheen === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+
+
+
+console.log("   ");
+
+const questionFiftheen = console.log("15. You feel confident that things will work out for you.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerFiftheen = prompt("Your answer: ").toUpperCase();
+
+if (answerFiftheen === "A") {
+console.log("You selected: AGREE");
+} else if (answerFiftheen === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+
+
+
+
+console.log("   ");
+
+const questionSixteen = console.log("16. Complex and novel ideas excite you more than simple and straightforward ones.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerSixteen = prompt("Your answer: ").toUpperCase();
+
+if (answerSixteen === "A") {
+console.log("You selected: AGREE");
+} else if (answerSixteen === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+
+
+console.log("   ");
+
+const questionSeventeen = console.log("17. Your living and working spaces are clean and organized.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerSeventeen = prompt("Your answer: ").toUpperCase();
+
+if (answerSeventeen === "A") {
+console.log("You selected: AGREE");
+} else if (answerSeventeen === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+
+
+
+
+console.log("   ");
+
+const questionEighteen = console.log("18. You usually stay calm, even under a lot of pressure.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerEighteen = prompt("Your answer: ").toUpperCase();
+
+if (answerEighteen === "A") {
+console.log("You selected: AGREE");
+} else if (answerEighteen === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+
+
+
+
+console.log("   ");
+
+const questionNintheen = console.log("19. You find the idea of networking or promoting yourself to strangers very daunting.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerNintheen = prompt("Your answer: ").toUpperCase();
+
+if (answerNintheen === "A") {
+console.log("You selected: AGREE");
+} else if (answerNintheen === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+
+
+
+console.log("   ");
+
+const questionTwenty = console.log("20. You usually feel more persuaded by what resonates emotionally with you than by factual arguments.");
+console.log("(A) AGREE");
+console.log("(B) DISAGREE");
+
+const answerTwenty = prompt("Your answer: ").toUpperCase();
+
+if (answerTwenty === "A") {
+console.log("You selected: AGREE");
+} else if (answerTwenty === "B") {
+console.log("You selected: DISAGREE");
+} else {
+console.log("Invalid choice! Please enter A or B.");
+ return;
+}
+
+
+console.log("     ");
+
+
+console.log("(A). AGREE (B). DISAGREE");
+console.log("   ");
+
+
+
+console.log("the options you selected are: ")
+
+console.log("   ");
+
+
+console.log("1. " + answerOne);
+console.log("2. " + answerTwo);
+console.log("3. " + answerThree);
+console.log("4. " + answerFour);
+console.log("5. " + answerFive);
+console.log("6. " + answerSix);
+console.log("7. " + answerSeven);
+console.log("8. " + answerEight);
+
+console.log("9. " + answerNine);
+console.log("10. " + answerTen);
+console.log("11. " + answerEleven);
+console.log("12. " + answerTwelve);
+console.log("13. " + answerThirteen);
+console.log("14. " + answerFourtheen);
+console.log("15. " + answerFiftheen);
+console.log("16. " + answerSixteen);
+
+console.log("17. " + answerSeventeen);
+console.log("18. " + answerEighteen);
+console.log("19. " + answerNintheen);
+console.log("20. " + answerTwenty);
+
+
+
+let introvert = "a".toUpperCase();
+let Extrovert = "b".toUpperCase();	
+
+if (answerOne === introvert || answerTwo === introvert ) {
+console.log("based on your answers you are an Extrovert");
+} else {
+console.log("based on your answers You are an introvert");
+} 
+
+
+
+
+
+}
+
+
+
+allQuestions();
+
+
